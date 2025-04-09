62b103d002e90f12746d353cbda752d962e7f1d7
messicaa <messica.pro@gmail.com>
2025-04-09 11:06:42 +0200

    version loader

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,30 @@ const getWeatherOfTheDay = () => {
   return "sunny";
 };
 
+const allData = [
+  {
+    id: 1,
+    title: "Lorem Ipsum",
+    content: "Lorem ipsum dolor sit amet",
+  },
+  {
+    id: 2,
+    title: "Schnapsum",
+    content: "Lorem Elsass ipsum Salut bisamme",
+  },
+  {
+    id: 3,
+    title: "Cupcake Ipsum",
+    content: "Tiramisu pastry wafer brownie soufflé",
+  },
+];
+
+type Data = typeof allData[0];
+
+const getSomeData = (id: number) => {
+  return allData.find((article) => article.id === id) as Data | null;
+};
+
 // router creation
 
 const router = createBrowserRouter([
@@ -35,6 +59,11 @@ const router = createBrowserRouter([
       {
         path: "/articles/:id",
         element: <Article />,
+        loader: ({ params }) => {
+          const idAsInt = parseInt(params.id ?? "0");
+
+          return getSomeData(idAsInt);
+        },
       },
     ],
   },
