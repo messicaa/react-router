32f2fe37475508c2d228843b5f1157f01cecd0ea
messicaa <messica.pro@gmail.com>
2025-04-09 11:03:26 +0200

    version useEffect

--- src/main.tsx
+++ src/main.tsx
@@ -7,20 +7,34 @@ import App from "./App";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Article from "./pages/Article";
+
+const getWeatherOfTheDay = () => {
+  return "sunny";
+};
 
 // router creation
 
 const router = createBrowserRouter([
   {
     element: <App />,
+    loader: () => {
+      return getWeatherOfTheDay();
+    },
+    id: "app",
     children: [
       {
         path: "/",
         element: <Home />,
+        
       },
       {
         path: "/about",
         element: <About />,
+      },
+      {
+        path: "/articles/:id",
+        element: <Article />,
       },
     ],
   },
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,11 @@
+
 function About() {
+  
 return <h1>Hello from About !</h1>;
 }
 
 export default About;
+
+
+
+
